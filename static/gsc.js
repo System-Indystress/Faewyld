f065ec725bc70d900a0d4feb7e055d8051b6ddf8
Matt Ahrens <matthew.ahrens@tufts.edu>
2018-05-28 02:27:36 -0400

    open close modal animation

--- static/gsc.js
+++ static/gsc.js
@@ -9,7 +9,18 @@ $("#closeModal").click(function(event) {
   $("#modal").addClass("closeAnimate");
   setTimeout(function(){
     $("#modal").removeClass("closeAnimate");
+    $("#modal").removeClass("show");
     $("#modal").addClass("hide");
+
   },1000);
 
+});
+
+$("#nav ul li").click(function(event) {
+  $("#modal").addClass("openAnimate");
+  setTimeout(function(){
+    $("#modal").removeClass("openAnimate");
+    $("#modal").removeClass("hide");
+    $("#modal").addClass("show");
+    },1000);
 });
